d1ca78ccf288c3f99eb00c8de030db1ebb72cfab
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2023-08-27 14:04:03 +0200

    Update 05_qiime2_import_PE.sh

--- 00_scripts/05_qiime2_import_PE.sh
+++ 00_scripts/05_qiime2_import_PE.sh
@@ -10,7 +10,7 @@ mkdir -p $OUTPUT
 # In the fastq manifest formats, a manifest file maps sample identifiers to fastq.gz or fastq absolute filepaths that contain sequence and quality data for the sample, and indicates the direction of the reads in each fastq.gz / fastq absolute filepath. The manifest file will generally be created by you, and it is designed to be a simple format that doesn’t put restrictions on the naming of the demultiplexed fastq.gz / fastq files, since there is no broadly used naming convention for these files. There are no restrictions on the name of the manifest file.
 # See https://docs.qiime2.org/2018.8/tutorials/importing/
 
-MANIFEST=/scratch_vol1/fungi/dugong_microbiome/98_database_files
+MANIFEST=/scratch_vol1/fungi/dugong_microbiome/98_database_files/manifest
 TMPDIR=/scratch_vol1
 
 ###############################################################
