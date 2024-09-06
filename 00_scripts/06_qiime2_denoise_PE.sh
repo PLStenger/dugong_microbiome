f6ffb4a76eec33137113da28da6d6ddf952fafb2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2024-09-06 08:56:38 +0200

    Update 06_qiime2_denoise_PE.sh

--- 00_scripts/06_qiime2_denoise_PE.sh
+++ 00_scripts/06_qiime2_denoise_PE.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
  
 
-WORKING_DIRECTORY=/scratch_vol1/fungi/dugong_microbiome/05_QIIME2
-OUTPUT=/scratch_vol1/fungi/dugong_microbiome/05_QIIME2/visual
+WORKING_DIRECTORY=/home/fungi/dugong_microbiome/05_QIIME2
+OUTPUT=/home/fungi/dugong_microbiome/05_QIIME2/visual
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
 
-METADATA=/scratch_vol1/fungi/dugong_microbiome/98_database_files/sample-metadata.tsv
+METADATA=/home/fungi/dugong_microbiome/98_database_files/sample-metadata.tsv
 # negative control sample :
-#NEG_CONTROL=/scratch_vol1/fungi/dugong_microbiome/98_database_files/Negative_control_Sample_RepSeq_V4.qza
-NEG_CONTROL=/scratch_vol1/fungi/dugong_microbiome/99_contamination
-#NEG_CONTROL=/scratch_vol1/fungi/dugong_microbiome/05_QIIME2/Original_reads_16S_ITS_18S_negative_control/core/RepSeq_negative_control.qza
+#NEG_CONTROL=/home/fungi/dugong_microbiome/98_database_files/Negative_control_Sample_RepSeq_V4.qza
+NEG_CONTROL=/home/fungi/dugong_microbiome/99_contamination
+#NEG_CONTROL=/home/fungi/dugong_microbiome/05_QIIME2/Original_reads_16S_ITS_18S_negative_control/core/RepSeq_negative_control.qza
 
-TMPDIR=/scratch_vol1
+TMPDIR=/home
 
 # https://chmi-sops.github.io/mydoc_qiime2.html
 
@@ -30,7 +30,7 @@ eval "$(conda shell.bash hook)"
 conda activate qiime2-2021.4
 
 # I'm doing this step in order to deal the no space left in cluster :
-export TMPDIR='/scratch_vol1/fungi'
+export TMPDIR='/home/fungi'
 echo $TMPDIR
 
 # dada2_denoise :
