cea7e0600761607fdf1cb5efe974b8cb3f1591e0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2024-09-06 09:08:26 +0200

    Update 01_quality_check_by_FastQC.sh

--- 00_scripts/01_quality_check_by_FastQC.sh
+++ 00_scripts/01_quality_check_by_FastQC.sh
@@ -6,8 +6,8 @@
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
 
-WORKING_DIRECTORY=/scratch_vol1/fungi/dugong_microbiome/01_raw_data
-OUTPUT=/scratch_vol1/fungi/dugong_microbiome/02_quality_check
+WORKING_DIRECTORY=/home/fungi/dugong_microbiome/01_raw_data
+OUTPUT=/home/fungi/dugong_microbiome/02_quality_check
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p $OUTPUT
